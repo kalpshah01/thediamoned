96e8aa85cd9dc149a5f70a4334714ce8bc8986c3
kalpshah01 <kalpshah962005@gmail.com>
2025-12-16 21:15:30 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -78,7 +78,7 @@
         <hr class="footer-divider" style="margin-top:22px">
 <br>
         <div class="text-center footer-copy" style="margin-bottom:-58px">
-            © <?php echo date('Y'); ?> DelishBite. All rights reserved.Kalp shah
+            © <?php echo date('Y'); ?> The Diamoned All rights reserved.Kalp shah
         </div>
     </div>
 </footer>
@@ -87,4 +87,5 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
 
